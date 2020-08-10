6d7d96f706a1096e2162f567b4e4a2ee48a34ede
Chhechinyong <chhechinyong01@gmail.com>
2020-08-10 19:17:00 +0700

    final :rocket:

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const btn1 = document.querySelector('#btn1');
-const name = document.querySelector('#name');
-const form =document.querySelector('#form');
-const popup = document.querySelector('#pop-up')
-const darkMode = document.querySelector('#dark-mode');
-const card = document.querySelectorAll('.card ');
-var count = 0;
 
+     // const darkMode = document.querySelector("#dark-mode");
+    const btn1 = document.querySelector('#btn1');
+    const name = document.querySelector('#name');
+    const form =document.querySelector('#form');
+    const popup = document.querySelector('#pop-up')
+    const darkMode = document.querySelector('#dark-mode');
+    const card = document.querySelectorAll('.card ');
+    const personalID =  document.querySelector('#personal-id')
+    var count = 0;
 
 
 darkMode.addEventListener('click', () => {
@@ -17,13 +19,17 @@ darkMode.addEventListener('click', () => {
         document.querySelector('.nav-link').style.color = "white"
         document.querySelector('#about').style.color = "white"
         document.querySelector('#contact').style.color = "white"
-        document.querySelector('#personal-id').style.color = "white"
+
+
+        if (personalID !== null) {
+        personalID.style.color = "white"
+        }
+
         darkMode.style.color ="white"
         for(var i = 0; i < card.length; i++){
            card[i].style.backgroundColor="#2d2d2d"
         }
 
-        // console.log('dark')
        
     }
     else{
@@ -32,11 +38,11 @@ darkMode.addEventListener('click', () => {
         document.querySelector('.nav-link').style.color = "black"
         document.querySelector('#about').style.color = "black"
         document.querySelector('#contact').style.color = "black"
-        document.querySelector("#dark-mode").style.color ="black"
+        darkMode.style.color ="black"
         for(var i = 0; i < card.length; i++){
             card[i].style.backgroundColor="white"
          }
-        document.querySelector('#personal-id').style.color = "#353b48"
+        // document.querySelector('#personal-id').style.color = "#353b48"
 
     }
     
@@ -47,22 +53,81 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   };
 
+if(form != null)
+{   
+    form.addEventListener('submit', (e)=> {
+        document.querySelector('#pop-up').style.visibility = "visible"
+            console.log('hello') 
+            e.preventDefault()
+                popup.innerHTML = `<div id ="alert" class="alert alert-success" role="alert">
+                <h4 class="alert-heading">Well done!</h4>
+                <img src="./image/tick.png" style = "width:1rem; height:1rem" >
+                </div>`        
+           
+            sleep(2000).then(() => {
+                document.querySelector('#pop-up').style.visibility = "hidden"
+                // btn1.innerHTML = `<button id="btn1" type="submit" class="btn form-control mt-3 btn-primary">Submit</button>` 
+            })
+        });
+}
+  
 
 
-// form.addEventListener('submit', (e)=> {
-//     document.querySelector('#pop-up').style.visibility = "visible"
-//         console.log('hello') 
-//         e.preventDefault()
-//             popup.innerHTML = `<div id ="alert" class="alert alert-success" role="alert">
-//             <h4 class="alert-heading">Well done!</h4>
-//             <img src="./tick.png" style = "width:1rem; height:1rem" >
-//             </div>`        
+
+
+
+
+// const btn1 = document.querySelector('#btn1');
+// const name = document.querySelector('#name');
+// const form =document.querySelector('#form');
+// const popup = document.querySelector('#pop-up')
+// const darkMode = document.querySelector('#dark-mode');
+// const card = document.querySelectorAll('.card ');
+// var count = 0;
+
+
+
+// darkMode.addEventListener('click', () => {
+//     count ++;
+//     if (count % 2 == 1){
+//         document.body.style.backgroundColor = "#2d2d2d"
+//         document.body.style.color="white"
+//         // card.style.backgroundColor = "#2d2d2d"
+//         document.querySelector('.nav-link').style.color = "white"
+//         document.querySelector('#about').style.color = "white"
+//         document.querySelector('#contact').style.color = "white"
+//         document.querySelector('#personal-id').style.color = "white"
+//         darkMode.style.color ="white"
+//         for(var i = 0; i < card.length; i++){
+//            card[i].style.backgroundColor="#2d2d2d"
+//         }
+
+//         // console.log('dark')
        
-//         sleep(2000).then(() => {
-//             document.querySelector('#pop-up').style.visibility = "hidden"
-//             // btn1.innerHTML = `<button id="btn1" type="submit" class="btn form-control mt-3 btn-primary">Submit</button>` 
-//         })
-//     });
+//     }
+//     else{
+//         document.body.style.backgroundColor ="white"
+//         document.body.style.color = "black"
+//         document.querySelector('.nav-link').style.color = "black"
+//         document.querySelector('#about').style.color = "black"
+//         document.querySelector('#contact').style.color = "black"
+//         document.querySelector("#dark-mode").style.color ="black"
+//         for(var i = 0; i < card.length; i++){
+//             card[i].style.backgroundColor="white"
+//          }
+//         document.querySelector('#personal-id').style.color = "#353b48"
+
+//     }
+    
+// });
+
+
+// function sleep(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+//   };
+
+
+
 
 
 
